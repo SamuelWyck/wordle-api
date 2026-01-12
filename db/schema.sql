d4b565a7b2769ea430a0c00074c3f8b6dc918de3
Samuel <samawyck@gmail.com>
2026-01-12 17:56:25 -0600

    Add tables for wordle sessions

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,17 @@ CREATE TABLE IF NOT EXISTS used_words (
     word VARCHAR(5) NOT NULL UNIQUE,
     date_used TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(3)
 );
+
+CREATE TABLE IF NOT EXISTS wordle_games (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    remaining_guesses INT DEFAULT 6 NOT NULL,
+    timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(3)
+);
+
+CREATE TABLE IF NOT EXISTS wordle_game_guesses (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    game_id UUID NOT NULL,
+    word VARCHAR(5) NOT NULL,
+    timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    CONSTRAINT "fk_game_id" FOREIGN KEY (game_id) REFERENCES wordle_games(id) ON DELETE CASCADE
+);
