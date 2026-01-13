c336243c259d27270495445d49170c3e15ab00c1
Samuel <samawyck@gmail.com>
2026-01-13 08:42:24 -0600

    Update remanining session guesses on guess

--- controllers/wordleController.js
+++ controllers/wordleController.js
@@ -3,6 +3,7 @@ const {validationResult} = require("express-validator");
 const {wordleWordGuessVal} = require("../utils/validator.js");
 const isValidWord = require("../utils/isValidWord.js");
 const wordManager = require("../utils/wordManager.js");
+const db = require("../db/queries.js");
 
 
 
@@ -11,15 +12,23 @@ const wordGuessGet = asyncHandler(async function(req, res) {
     if (!errors.isEmpty()) {
         return res.status(400).json({errors: errors.array()});
     }
+    if (req.wordleSession.remaining_guesses === 0) {
+        return res.status(404).json({errors: [{msg: "Out of guesses"}]});
+    }
     const word = req.params.word;
     const validWord = await isValidWord(word);
     if (!validWord) {
-        return res.status(404).json({errors: [{msg: "Not a real English word."}]});
+        return res.status(404).json({errors: [{msg: "Not a real English word"}]});
     }
 
+    const updatedGuesses = req.wordleSession.remaining_guesses - 1;
     let score = null;
     try {
-        score = await wordManager.testGuess(word);
+        let [wordScore, _] = await Promise.all([
+            wordManager.testGuess(word),
+            db.updateWordleSessionGuesses(req.wordleSession.id, updatedGuesses)
+        ]);
+        score = wordScore;
     } catch {
         return res.status(500).json({errors: [{msg: "Server error"}]});
     }
