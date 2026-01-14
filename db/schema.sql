616471932b4c044604ff30d553cbc5a897e8c753
Samuel <samawyck@gmail.com>
2026-01-14 15:13:45 -0600

    Add all words table

--- db/schema.sql
+++ db/schema.sql
@@ -17,3 +17,8 @@ CREATE TABLE IF NOT EXISTS wordle_game_guesses (
     timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     CONSTRAINT "fk_game_id" FOREIGN KEY (game_id) REFERENCES wordle_games(id) ON DELETE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS all_words (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    word VARCHAR(5) NOT NULL UNIQUE
+);
