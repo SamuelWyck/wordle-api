a19a01a125aff8bc5479eba7b9001a2e905441ae
Samuel <samawyck@gmail.com>
2026-01-13 18:01:39 -0600

    Use cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const utilRoute = require("./routes/utilRoute.js");
 const wordleRoute = require("./routes/wordleRoute.js");
@@ -7,7 +8,7 @@ const wordleRoute = require("./routes/wordleRoute.js");
 
 const app = express();
 
-
+app.use(cors());
 
 app.get("/", function(req, res) {
     return res.json({msg: "hi"});
