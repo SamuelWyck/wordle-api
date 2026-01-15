98500a30eefa243d70991664b79f0513a17c6abe
Samuel <samawyck@gmail.com>
2026-01-15 16:41:13 -0600

    Add fall-through route and error route

--- app.js
+++ app.js
@@ -12,13 +12,20 @@ app.use(cors({
     exposedHeaders: process.env.SET_WORDLE_SESSION_HEADER
 }));
 
+
 app.get("/", function(req, res) {
     return res.json({msg: "hi"});
 });
-
 app.use("/util", utilRoute);
 app.use("/wordle", wordleRoute);
 
+app.use(function(req, res) {
+    return res.status(404).json({errors: [{msg: "Page not found"}]});
+});
+app.use(function(error, req, res, next) {
+    console.log(error);
+    return res.status(500).json({errors: [{msg: "Server error"}]});
+});
 
 
 const PORT = process.env.PORT;
