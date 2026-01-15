e11d9a6eaa87415f157e118a1bb07c5631f8f847
Samuel <samawyck@gmail.com>
2026-01-15 16:31:46 -0600

    Remove commented out code

--- utils/isValidWord.js
+++ utils/isValidWord.js
@@ -1,16 +1,5 @@
 const db = require("../db/queries.js");
 
-// async function isValidWord(word) {
-//     const url = `${process.env.WORD_CHECK_API_URL}/${word}?key=${process.env.WORD_CHECK_API_KEY}`;
-//     const res = await fetch(url);
-//     const parsedRes = await res.json();
-
-//     if (parsedRes.length === 0) {
-//         return false;
-//     }
-//     const firstResult = parsedRes[0];
-//     return typeof firstResult !== "string";
-// };
 
 
 async function isValidWord(word) {
