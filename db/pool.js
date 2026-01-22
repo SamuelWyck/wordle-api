a734e07c0c7ff31b798b8673402dec03bd5a163f
Samuel <samawyck@gmail.com>
2026-01-22 12:50:28 -0600

    Create client for mongodb

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,22 @@
-const {Pool} = require("pg");
+const {MongoClient} = require("mongodb");
 
 
 
-const pool = new Pool({
-    connectionString: process.env.DB_CONNECTION_URL
-});
+const client = new MongoClient(process.env.MONGODB_URL);
+const database = client.db("wordle");
 
 
 
-module.exports = pool;
+module.exports = database;
+
+// const {Pool} = require("pg");
+
+
+
+// const pool = new Pool({
+//     connectionString: process.env.DB_CONNECTION_URL
+// });
+
+
+
+// module.exports = pool;
